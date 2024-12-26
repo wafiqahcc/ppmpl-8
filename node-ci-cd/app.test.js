645797499c1867a8924fc3979992eb3c6a22ec52
fiqaha <155350529+fiqaha@users.noreply.github.com>
2024-12-27 00:52:47 +0700

    https://github.com/wafiqahcc/ppmpl-8.git

---
+++ node-ci-cd/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('./app');
+describe('GET /', () => {
+ it('responds with Hello, World!', async () => {
+ const res = await request(app).get('/');
+ expect(res.statusCode).toBe(200);
+ expect(res.text).toBe('Hello, World!');
+ });
+});
